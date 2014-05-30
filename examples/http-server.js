782cc49073b8441d12de2342200a495991b196ef
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-05-30 11:01:05 -0700

    Cleans up example.

--- examples/http-server.js
+++ examples/http-server.js
@@ -1,20 +1,19 @@
-
-var logentries = require('node-logentries')
+var http = require('http');
+var logentries = require('..');
 
 var log = logentries.logger({
-  token:'YOUR_TOKEN'
+  token: 'YOUR_TOKEN'
 })
 
-log.on('error',function(err){
-  console.log('LOG ERROR: ', err)
+log.on('error', function(err) {
+  console.log('LOG ERROR: ', err);
 })
 
-var http = require('http');
 http.createServer(function (req, res) {
-
-  log.info( req.connection.remoteAddress+', '+req.method+', '+req.url)
-
+  log.info(req.connection.remoteAddress + ', ' + req.method + ', ' + req.url);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
+})
+.listen(1337, "127.0.0.1");
+
 console.log('Server running at http://127.0.0.1:1337/');
